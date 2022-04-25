a19f114d3871e56374a6e3146ba4fd9b37e15914
betsalel koginsky <betsalelkogo@gmail.com>
2022-04-25 12:34:25 +0300

    remove search bar from html

--- product.js
+++ product.js
@@ -1,4 +1,12 @@
-let product = localStorage.getItem("product");
-console.log(product);
-//var resetValue = 0;
-//localStorage.setItem("product", resetValue);
+const type = document.getElementById("type_d");
+const name = document.getElementById("product_page_name");
+const price = document.getElementById("product_page_price");
+const description = document.getElementById("description_d");
+const manufacturer = document.getElementById("manufactur_d");
+const max_speed = document.getElementById("max_speed_d");
+const weight = document.getElementById("weight_d");
+const wheel_size = document.getElementById("wheel_size_d");
+const quantity = document.getElementById("quantetity_d");
+const userName = document.getElementById("navbar_profile_name");
+const search = document.getElementById("searchgroup");
+search.remove();
