4a1ea8b2ee56a5bfb6ea5ccee939250f74033828
Mikhadi <73593882+Mikhadi@users.noreply.github.com>
2022-04-26 12:14:55 +0300

    Shop updated

---
+++ shop_register.js
@@ -0,0 +1,32 @@
+let store_name = document.getElementById("store_name")
+var sunday = document.getElementById("sunday_check")
+let sunday_from = document.getElementById("sunday_from")
+let sunday_till = document.getElementById("sunday_till")
+let monday = document.getElementById("monday_check")
+let monday_from = document.getElementById("monday_from")
+let monday_till = document.getElementById("monday_till")
+let tuesday = document.getElementById("tuesday_check")
+let tuesday_from = document.getElementById("tuesday_from")
+let tuesday_till = document.getElementById("tuesday_till")
+let wednesday = document.getElementById("wednesday_check")
+let wednesday_from = document.getElementById("wednesday_from")
+let wednesday_till = document.getElementById("wednesday_till")
+let thursday = document.getElementById("thursday_check")
+let thursday_from = document.getElementById("thursday_from")
+let thursday_till = document.getElementById("thursday_till")
+let friday = document.getElementById("friday_check")
+let friday_from = document.getElementById("friday_from")
+let friday_till = document.getElementById("friday_till")
+let saturday = document.getElementById("saturday_check")
+let saturday_from = document.getElementById("saturday_from")
+let saturday_till = document.getElementById("saturday_till")
+
+function save(){
+    if(sunday.checked == true){
+        alert("true")
+    }
+    else{
+        alert("false")
+    }
+}
+
