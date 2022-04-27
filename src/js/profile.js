6c65ad70cd908fe58929ca136cbb724d98940d98
yossi iluz <yossieluz1@gmail.com>
2022-04-27 15:29:35 +0300

    btn change

--- src/js/profile.js
+++ src/js/profile.js
@@ -9,7 +9,16 @@ const updateNavBar = () => {
   if (user.wishList.length > 0) wish_list.style = "color: red";
   if (user.orderList.length > 0) shopping_cart.style = "color: red";
 };
-updateNavBar();
+const edit_btn = document.getElementById("edit_btn");
+const save_btn = document.getElementById("save_btn");
+edit_btn.addEventListener('click',()=>{
+  save_btn.disabled=false;
+  email.disabled=false;
+  username.disabled=false;
+  phone_number.disabled=false;
+  edit_btn.disabled=true;
+})
+
 let username = document.getElementById("username");
 let phone_number = document.getElementById("phone_number");
 let email = document.getElementById("email");
@@ -18,5 +27,10 @@ const updateDetails = () => {
   username.value = user.name;
   phone_number.value = user.phone;
   email.value = user.email;
+  email.disabled=true;
+  username.disabled=true;
+  phone_number.disabled=true;
+  save_btn.disabled=true;
 };
 updateDetails();
+updateNavBar();
