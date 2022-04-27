4cd5c30ad835142e4c52f91e0905cd021cbae876
Mikhadi <73593882+Mikhadi@users.noreply.github.com>
2022-04-27 16:22:32 +0300

    shop functionality added

---
+++ src/js/shop_dashboard.js
@@ -0,0 +1,5 @@
+document.getElementById("add_item_btn").addEventListener("mouseover", function(){
+    document.getElementById("sells").textContent="Wrong"
+    document.getElementById("earnings").textContent="Wrong"
+});
+
