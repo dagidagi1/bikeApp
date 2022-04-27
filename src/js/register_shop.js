7af7c18ff0e16e246e93077f0ae377bd14ba955f
betsalel koginsky <betsalelkogo@gmail.com>
2022-04-27 18:44:15 +0300

    hh

--- src/js/register_shop.js
+++ src/js/register_shop.js
@@ -1,9 +1,11 @@
-var sName = document.getElementById("create_store_name");
-var hours = document.getElementById("create_store_tb");
+let sName = document.getElementById("create_store_name");
+let hours = document.getElementById("create_store_tb");
 document
   .getElementById("create_store_submit_btn")
   .addEventListener("click", function () {
     console.log(hours);
+    location.replace("shop_dashboard.html");
   });
 const search = document.getElementById("searchgroup");
 search.remove();
+create_store_submit_btn;
