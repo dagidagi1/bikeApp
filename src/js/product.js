c33c75764c7516952be052e72cd3953aedff39d8
betsalel koginsky <betsalelkogo@gmail.com>
2022-05-02 13:46:10 +0300

    product connect btns

--- src/js/product.js
+++ src/js/product.js
@@ -3,6 +3,7 @@ var parametrs = location.search.substring(1).split("&");
 var temp = parametrs[0].split("=");
 const index_p = decodeURI(temp[1]);
 let data = [];
+let cur_user;
 const type = document.getElementById("type_d");
 const name = document.getElementById("product_page_name");
 const price = document.getElementById("product_page_price");
@@ -17,6 +18,14 @@ const userNameNavBar = document.getElementById("navbar_profile_name");
 const wish_list = document.getElementById("wish_list");
 const shopping_cart = document.getElementById("shopping_cart");
 const img = document.getElementById("product_page_image");
+const add_wish_btn = document.getElementById("wish_btn");
+const add_cart_btn = document.getElementById("add_to_card");
+add_cart_btn.addEventListener("click", () => {
+  addToCart();
+});
+add_wish_btn.addEventListener("click", () => {
+  addToWishList();
+});
 search.remove();
 const updateNavBar = () => {
   fbAuth.onAuthStateChanged((user) => {
@@ -28,6 +37,8 @@ const updateNavBar = () => {
         .get()
         .then((snapshot) => {
           snapshot.forEach((doc) => {
+            cur_user = doc.data();
+            console.log(cur_user);
             userNameNavBar.innerText = doc.data().name;
             if (doc.data().wishList.length > 0) wish_list.style = "color: red";
             if (doc.data().orderList.length > 0)
@@ -48,7 +59,7 @@ dbProducts.get().then((querySnapshot) => {
 const updateDescriptions = (data) => {
   type.innerText = data.type === 0 ? "Bycicle" : "Scooter";
   name.innerText = data.name;
-  price.innerText = data.price;
+  price.innerText = data.price + "$";
   description.innerText = data.description;
   manufacturer.innerText = data.manufacturer;
   max_speed.innerText = data.max_speed;
@@ -58,4 +69,11 @@ const updateDescriptions = (data) => {
     data.type === 0
       ? "assets/img/200829b1-9d17-4b9b-8bf8-36baba8859e6.jpg"
       : "assets/img/snimok6.png";
+};
+const addToCart = () => {
+  //need to call firebase to update user
+  cur_user.orderList.push(index_p);
+};
+const addToWishList = () => {
+  cur_user.wishList.push(index_p);
 };
