58bc8de3cb12e4956964b6acbec27544f5c9906e
Mikhadi <73593882+Mikhadi@users.noreply.github.com>
2022-05-02 10:51:35 +0300

    store functionality added

--- src/js/shop_add_item.js
+++ src/js/shop_add_item.js
@@ -1,16 +1,51 @@
-v_category = document.getElementById("category")
-v_type = document.getElementById("vehicle_type")
-v_name = document.getElementById("vehicle_name")
-price = document.getElementById("price")
-manufacturer = document.getElementById("manufacturer")
-max_speed = document.getElementById("max_speed")
-weight = document.getElementById("weight")
-wheel_size = document.getElementById("wheel_size")
-quantity = document.getElementById("quantity")
-description = document.getElementById("description")
-add_item_btn = document.getElementById("add_item_btn")
+import { dbProducts, fbAuth, dbUsers, dbStores } from "../firebase/data.js";
 
-add_item_btn.addEventListener('click', function(){
-    alert("Something")
-})
+const v_category = document.getElementById("category")
+const v_type = document.getElementById("vehicle_type")
+const v_name = document.getElementById("vehicle_name")
+const price = document.getElementById("price")
+const manufacturer = document.getElementById("manufacturer")
+const max_speed = document.getElementById("max_speed")
+const weight = document.getElementById("weight")
+const wheel_size = document.getElementById("wheel_size")
+const quantity = document.getElementById("quantity")
+const description = document.getElementById("description")
+const add_item_btn = document.getElementById("add_item_btn")
+var shop_id = null
 
+add_item_btn.addEventListener("click", function () {
+    fbAuth.onAuthStateChanged((user) => {
+        if (user) {
+            dbUsers.where("email", "==", user.email).get()
+            .then((snapshot) => {
+                snapshot.forEach((doc) => {
+                    shop_id = doc.data().store;
+                });
+                dbProducts.add({
+                    store_id: shop_id,
+                    category: v_category.selectedIndex,
+                    type: v_type.selectedIndex,
+                    name: v_name.value,
+                    price: price.value,
+                    manufacturer: manufacturer.value,
+                    max_speed: max_speed.value,
+                    weight: weight.value,
+                    wheel_size: wheel_size.value,
+                    quantity: quantity.value,
+                    description: description.value,
+                }).then((docRef) => {
+                    const doc_id = docRef.id
+                    var storeRef = dbStores.doc(shop_id)
+                    storeRef.update({
+                        products: firebase.firestore.FieldValue.arrayUnion(doc_id)
+                    });
+                    // location.replace("registered_home.html" + '?id='+shop_id);
+                })
+                .catch((error) => {
+                    console.error("Error adding document: ", error);
+                });
+            })
+            
+        }
+        });
+    });
