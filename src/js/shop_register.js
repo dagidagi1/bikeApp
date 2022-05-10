21b7121f6abcfb00c1cf02e7efef6fd1b27f3f2f
Mikhadi <73593882+Mikhadi@users.noreply.github.com>
2022-05-10 10:54:20 +0300

    form validation register store

--- src/js/shop_register.js
+++ src/js/shop_register.js
@@ -1,5 +1,6 @@
 import { fbAuth, dbUsers, dbStores } from "../firebase/data.js";
 
+const redColor = "#e74a3b";
 const DAYS_OF_WEEK = [
   "Sunday",
   "Monday",
@@ -14,7 +15,16 @@ const storeName = document.getElementById("store_name");
 let userId = null;
 let shopId = null;
 
-document.getElementById("create_btn").addEventListener("click", function () {
+document.getElementById("create_btn").addEventListener("click", function(){
+  if(formValidation() == false){
+    return;
+  }
+  else{
+    saveStore();
+  }
+})
+
+function saveStore() {
   const workHours = new Map();
   DAYS_OF_WEEK.forEach((d) => {
     if (document.getElementById(d).checked == false) {
@@ -62,4 +72,37 @@ document.getElementById("create_btn").addEventListener("click", function () {
         });
     }
   });
-});
+};
+
+function formValidation(){
+  if(storeName.value == null || storeName.value == ""){
+    storeName.style.borderColor = redColor;
+    return false;
+  }
+  let flag = true
+  DAYS_OF_WEEK.forEach((d)=>{
+    var from = parseInt(document.getElementById(`${d}_from`).value);
+    var till = parseInt(document.getElementById(`${d}_till`).value);
+    if(from < 0 || from >24 || till < 0 || till > 24 || till <= from){
+      console.log(d, from, till);
+      if(from < 0 || from >24){
+        document.getElementById(`${d}_from`).style.borderColor = redColor;
+        flag = false;
+      }
+      if(till < 0 || till > 24){
+        document.getElementById(`${d}_till`).style.borderColor = redColor;
+        flag = false;
+      }
+      if(till <= from){
+        document.getElementById(`${d}_till`).style.borderColor = redColor;
+        document.getElementById(`${d}_from`).style.borderColor = redColor;
+        flag = false;
+      }
+    }
+    else{
+      document.getElementById(`${d}_till`).style.borderColor = "";
+      document.getElementById(`${d}_from`).style.borderColor = "";
+    }
+  })
+  return flag;
+}
