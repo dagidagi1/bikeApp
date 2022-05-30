3620e4f04086e18dc53351f3e71491435f5e5f69
betsalel koginsky <betsalelkogo@gmail.com>
2022-05-30 10:03:52 +0300

    jjj

--- src/js/orderList.js
+++ src/js/orderList.js
@@ -4,11 +4,12 @@ import {
   dbOrders,
   dbProducts,
   storageRef,
-} from '../firebase/data.js';
-const rateOrderText = document.getElementById('rateOrderText');
-const rateOrder = document.getElementById('rateOrder');
-const submitReviewBtn = document.getElementById('submit_review');
-const search = document.getElementById('searchgroup');
+} from "../firebase/data.js";
+const rateOrderText = document.getElementById("rateOrderText");
+const rateOrder = document.getElementById("rateOrder");
+const submitReviewBtn = document.getElementById("submit_review");
+const search = document.getElementById("searchgroup");
+const orderList = document.getElementById("orderList");
 search.remove();
 var orders = [];
 var data = [];
@@ -16,58 +17,67 @@ var user;
 fbAuth.onAuthStateChanged((u) => {
   if (u) {
     dbUsers
-        .where('email', '==', u.email)
-        .get()
-        .then((snapshot) => {
-          snapshot.forEach((doc) => {
-            user = doc.data();
-          });
-          loadData();
+      .where("email", "==", u.email)
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          user = doc.data();
         });
+        loadorders();
+      });
   }
 });
 const loadorders = () => {
   let i = 0;
   dbOrders
-      .where('email', '==', user.email)
-      .get()
-      .then((querySnapshot) => {
+    .where("buyer", "==", user.email)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        orders.push(doc.data());
+        orders[i].id = doc.id;
+        i++;
+      });
+      dbProducts.get().then((querySnapshot) => {
+        i = 0;
         querySnapshot.forEach((doc) => {
-          orders.push(doc.data());
-          orders[i].id = data.filter((d) => {
-            if (d.name === orders[i].nameProduct) return d.id;
-          });
-          i++;
+          for (let j = 0; j < orders.length; j++) {
+            if (doc.id === orders[j].prodId) {
+              data.push(doc.data());
+              data[i].id = doc.id;
+              i++;
+            }
+          }
         });
         init();
       });
+    });
 };
-const orderList = document.getElementById('orderList');
-const makeRowOrder = (ord) => {
-  if (ord.status === 'Shipping') {
+const makeRowOrder = (ord, da) => {
+  if (ord.status === "Shipping") {
     return `<tr>
-    <td>${ord.order_number}</td>
+    <td>${ord.prodId}</td>
     <td>
       <img
-      id="${ord.order_number}"
+      id="${ord.prodId}"
         src=''
         width="80px"
       />
     </td>
-    <td>${ord.nameProduct}</td>
+    <td>${da.name}</td>
     <td style="color: var(--bs-info)">${ord.status}</td>
     <td class="text-end"></td>
   </tr>`;
-  } else if (ord.status === 'Waiting') {
-    return `<td>${ord.order_number}</td>
+  } else if (ord.status === "Waiting") {
+    return `<td>${ord.prodId}</td>
     <td>
       <img
-      id="${ord.order_number}"
+      id="${ord.prodId}"
       src=''
         width="80px"
       />
     </td>
-    <td>${ord.nameProduct}</td>
+    <td>${da.name}</td>
     <td style="color: var(--bs-danger)">${ord.status}</td>
     <td class="text-end">
       <button
@@ -79,50 +89,50 @@ const makeRowOrder = (ord) => {
           color: var(--bs-table-striped-color);
           border-style: hidden;
         "
-        value="${ord.order_number}"
+        value="${ord.prodId}"
       >
         Cancel order&nbsp;<i class="fa fa-ban"></i>
       </button>
     </td>
   </tr>`;
-  } else if (ord.status === 'Cancelled') {
-    return `<td>${ord.order_number}</td>
+  } else if (ord.status === "Cancelled") {
+    return `<td>${ord.prodId}</td>
     <td>
       <img
-      id="${ord.order_number}"
+      id="${ord.prodId}"
       src=''
         width="80px"
       />
     </td>
-    <td>${ord.nameProduct}</td>
+    <td>${da.name}</td>
     <td style="color: var(--bs-success)">${ord.status}</td>
     <td class="text-end">
     </td>
   </tr>`;
   } else if (ord.review) {
-    return `<td>${ord.order_number}</td>
+    return `<td>${ord.prodId}</td>
     <td>
       <img
-      id="${ord.order_number}"
+      id="${ord.prodId}"
       src=''
         width="80px"
       />
     </td>
-    <td>${ord.nameProduct}</td>
+    <td>${da.name}</td>
     <td style="color: var(--bs-success)">${ord.status}</td>
     <td class="text-end">
     </td>
   </tr>`;
   } else {
-    return `<td>${ord.order_number}</td>
+    return `<td>${ord.prodId}</td>
   <td>
     <img
-    id="${ord.order_number}"
+    id="${ord.prodId}"
     src=''
       width="80px"
     />
   </td>
-  <td>${ord.nameProduct}</td>
+  <td>${da.name}</td>
   <td style="color: var(--bs-success)">${ord.status}</td>
   <td class="text-end">
     <button
@@ -133,7 +143,7 @@ const makeRowOrder = (ord) => {
         background: var(--bs-table-bg);
         color: var(--bs-table-striped-color);
         border-style: hidden;"
-      value="${ord.order_number}"
+      value="${ord.prodId}"
       data-bs-target="#modal-2"
               data-bs-toggle="modal"
 
@@ -145,62 +155,57 @@ const makeRowOrder = (ord) => {
   }
 };
 const CancelOrder = (e) => {
+  console.log("!@#$");
   orders.map((o) => {
-    if (o.order_number === e) {
-      o.status = 'Cancelled';
-      dbOrders.doc(o.order_number).set(o);
+    if (o.prodId === e) {
+      o.status = "Cancelled";
+      dbOrders.doc(o.id).set(o);
       init();
     }
   });
 };
 const feedBack = (e) => {
   orders.map((o) => {
-    if (o.order_number === e) {
+    if (o.prodId === e) {
       o.rate = rateOrder.value;
       o.rateOrderText = rateOrderText.value;
       o.review = true;
-      dbOrders.doc(o.order_number).set(o);
+      dbOrders.doc(o.prodId).set(o);
     }
   });
 };
-submitReviewBtn.addEventListener('click', () => {
-  feedBack(document.getElementById('BtnfeedBack').value);
+submitReviewBtn.addEventListener("click", () => {
+  feedBack(document.getElementById("BtnfeedBack").value);
   init();
 });
 const init = () => {
-  orderList.innerHTML = '';
-  for (let i = 0; i < orders.length; i++) {
-    orderList.innerHTML += makeRowOrder(orders[i], i);
-    if (orders[i].id[0].hasImg) {
-      storageRef
-          .child(orders[i].id[0].id)
-          .getDownloadURL()
-          .then((url) => {
-          // Or inserted into an <img> element
-            const img = document.getElementById(`${orders[i].order_number}`);
-            img.src = url;
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-    } else {
-      const img = document.getElementById(`${orders[i].order_number}`);
-      img.src = orders[i].src;
+  orderList.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < orders.length; j++) {
+      if (data[i].id === orders[j].prodId) {
+        orderList.innerHTML += makeRowOrder(orders[j], data[i]);
+        if (data[i].hasImg) {
+          storageRef
+            .child(data[i].id)
+            .getDownloadURL()
+            .then((url) => {
+              // Or inserted into an <img> element
+              const img = document.getElementById(`${orders[j].prodId}`);
+              img.src = url;
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        } else {
+          const img = document.getElementById(`${orders[j].prodId}`);
+          img.src = data[i].src;
+        }
+        break;
+      }
     }
   }
-  document.getElementById('btncancel')?.addEventListener('click', () => {
-    CancelOrder(document.getElementById('btncancel').value);
+  document.getElementById("btncancel")?.addEventListener("click", () => {
+    CancelOrder(document.getElementById("btncancel").value);
     init();
   });
 };
-function loadData() {
-  dbProducts.get().then((querySnapshot) => {
-    let i = 0;
-    querySnapshot.forEach((doc) => {
-      data.push(doc.data());
-      data[i].id = doc.id;
-      i++;
-    });
-    loadorders();
-  });
-}
